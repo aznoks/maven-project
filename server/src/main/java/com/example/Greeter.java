5b8d15bb82936ba473c1f43c2876346159ce7714
aznoks <34538124+aznoks@users.noreply.github.com>
2020-04-28 07:35:22 +0300

    Add files

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+/**
+ * @param someone the name of a person
+ * @return greeting string
+ **/
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
